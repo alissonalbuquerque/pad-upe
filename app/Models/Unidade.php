983c2ceb9f135f42afa07488379065e15711f8cc
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2022-03-11 09:49:31 -0300

    add model de Unidade

--- app/Models/Unidade.php
+++ app/Models/Unidade.php
@@ -5,37 +5,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Unidade extends Model
-{
-    const ARCOVERDE = 1;
-    const CARUARU = 2;
-    const GARANHUNS = 3;
-    const NAZARE_DA_MATA = 4;
-    const PALMARES = 5;
-    const PETROLINA = 6;
-    const RECIFE = 7;
-    const REGIAO_METROPOLITANA = 8;
-    const SALGUEIRO = 9;
-    const SERRA_TALHADA = 10;
+{   
 
     /**
-     * @param integer $value
-     * @return array|string
+     * References table unidade
+     * 
+     * @var string
      */
-    public static function listUnidades($value = null)
-    {
-        $values = [
-            self::ARCOVERDE => 'Arcoverde',
-            self::CARUARU => 'Caruaru',
-            self::GARANHUNS => 'Garanhuns',
-            self::NAZARE_DA_MATA => 'Nazaré da Mata',
-            self::PALMARES => 'Palmares',
-            self::PETROLINA => 'Petrolina',
-            self::RECIFE => 'Recife',
-            self::REGIAO_METROPOLITANA => 'Região Metropolitana',
-            self::SALGUEIRO => 'Salgueiro',
-            self::SERRA_TALHADA => 'Serra Talhada',        
-        ];
+    protected $table = "unidade";
 
-        return $value != null? $values[$value] : $values;
+
+    /**
+     * The attributes that are mass assignable.
+     * 
+     * @var array
+     */
+    protected $fillable = ['name'];
+    
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->name;
     }
+    
 }
