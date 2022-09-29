b46cbe8883e3ca0cbd71e042483d4921bd532804
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2022-09-28 22:04:21 -0300

    add tables de outros

---
+++ database/migrations/2022_09_29_005909_create_extensao_outro_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExtensaoOutroTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('extensao_outro', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_pad_id')->notNull();
+            $table->tinyInteger('dimensao')->notNull();
+            $table->string('cod_atividade')->notNull();
+            $table->string('atividade')->notNull();
+            $table->text('descricao')->notNull();
+            $table->integer('ch_semanal')->notNull();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('extensao_outro');
+    }
+}
