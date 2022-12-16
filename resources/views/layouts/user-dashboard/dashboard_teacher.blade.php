dc63eeec3f520c23082af64f5375efe2341be5f5
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2022-12-16 03:55:38 -0300

    add correções

--- resources/views/layouts/user-dashboard/dashboard_teacher.blade.php
+++ resources/views/layouts/user-dashboard/dashboard_teacher.blade.php
@@ -1,7 +1,3 @@
-@php
-    use App\Models\Pad;
-@endphp
-
 <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Bem Vindo ao PAD</h1>
@@ -25,34 +21,5 @@
                 </div>
             </div>
         @endforeach
-    </div>
-</div>
-
-<div class="tab-pane" id="pad" role="tabpanel" aria-labelledby="pad-tab">
-    <div
-        class="d-flex flex-column justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">Dimenções-PAD</h1>
-    </div>
-    <div class="d-flex justify-content-start ">
-        <div class="card text-center" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-        <div class="card text-center" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-            </div>
-        </div>
-        <div class="card text-center" style="width: 18rem;">
-            <div class="card-body">
-                <h5 class="card-title">Special title treatment</h5>
-                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
     </div>
 </div>
