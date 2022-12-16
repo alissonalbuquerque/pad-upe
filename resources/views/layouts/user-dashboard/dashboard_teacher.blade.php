17866b370786c5e3d24735a874b6a7f8dc31dbca
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2022-12-16 03:50:35 -0300

    add correções

--- resources/views/layouts/user-dashboard/dashboard_teacher.blade.php
+++ resources/views/layouts/user-dashboard/dashboard_teacher.blade.php
@@ -1,3 +1,9 @@
+@php
+    use App\Models\Pad;
+
+    dd('pad');
+@endphp
+
 <div class="tab-pane active" id="home" role="tabpanel" aria-labelledby="home-tab">
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">Bem Vindo ao PAD</h1>
