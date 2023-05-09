fb2ec161fadbea108d437cb3b1a7b5576bdddf62
Yuri Resende <yresendeo@gmail.com>
2023-05-09 18:17:41 -0300

    Corrigindo o calculo da carga horária

--- app/Http/Controllers/PadController.php
+++ app/Http/Controllers/PadController.php
@@ -321,11 +321,11 @@ class PadController extends Controller
             $userPad = $professor->userPads()->where('pad_id', '=', $pad->id)->first();
 
             $avaliacoes = $this->get_avaliacoes($userPad, $avaliador_pad); 
-            $avaliacoes_ensino   = $avaliacoes['ensino'];
-            $avaliacoes_pesquisa = $avaliacoes['pesquisa'];
-            $avaliacoes_extensao = $avaliacoes['extensao'];
-            $avaliacoes_gestao   = $avaliacoes['gestao'];
-
+            $avaliacoes_ensino   = $avaliacoes['ensino']->get();
+            $avaliacoes_pesquisa = $avaliacoes['pesquisa']->get();
+            $avaliacoes_extensao = $avaliacoes['extensao']->get();
+            $avaliacoes_gestao   = $avaliacoes['gestao']->get();
+            
             if( $avaliacoes_ensino->all() != null || 
                 $avaliacoes_pesquisa->all() != null || 
                 $avaliacoes_extensao->all() != null || 
@@ -335,7 +335,6 @@ class PadController extends Controller
             }
 
             $professor->ch = $this->get_carga_horaria_total($avaliacoes);
-
         }
 
         return view("pad.avaliacao.professores", compact('professores', 'pad', 'index_menu'));
@@ -362,11 +361,11 @@ class PadController extends Controller
         $status = Status::listStatus();
 
         $avaliacoes = $this->get_avaliacoes($user_pad, $avaliador_pad); 
-        $avaliacoes_ensino   = $avaliacoes['ensino'];
-        $avaliacoes_pesquisa = $avaliacoes['pesquisa'];
-        $avaliacoes_extensao = $avaliacoes['extensao'];
-        $avaliacoes_gestao   = $avaliacoes['gestao'];
-
+        $avaliacoes_ensino   = $avaliacoes['ensino']->paginate(5);
+        $avaliacoes_pesquisa = $avaliacoes['pesquisa']->paginate(5);
+        $avaliacoes_extensao = $avaliacoes['extensao']->paginate(5);
+        $avaliacoes_gestao   = $avaliacoes['gestao']->paginate(5);
+        
         //Informando quais tipos (ensino, pesquisa, extensão ou gestão) de atividades podem ser avaliadas pelo usuário logado.
         $avalPad = $user->avaliadorPad()->first();
 
@@ -462,7 +461,7 @@ class PadController extends Controller
                 $avaliacoes_ensino_ids = array_merge($avaliacoes_ensino_ids, $avaliacao_ids);
             }
                         
-            $avaliacoes_ensino = Avaliacao::whereIn('id', $avaliacoes_ensino_ids)->orderBy('status')->paginate(5);
+            $avaliacoes_ensino = Avaliacao::whereIn('id', $avaliacoes_ensino_ids)->orderBy('status');
             //
         }
 
@@ -495,7 +494,7 @@ class PadController extends Controller
                 $avaliacoes_pesquisa_ids = array_merge($avaliacoes_pesquisa_ids, $avaliacao_ids);
             }
                         
-            $avaliacoes_pesquisa = Avaliacao::whereIn('id', $avaliacoes_pesquisa_ids)->orderBy('status')->paginate(5);
+            $avaliacoes_pesquisa = Avaliacao::whereIn('id', $avaliacoes_pesquisa_ids)->orderBy('status');
         }
 
         if (in_array(Dimensao::EXTENSAO, $dimensoes)) {
@@ -523,7 +522,7 @@ class PadController extends Controller
                 $avaliacoes_extensao_ids = array_merge($avaliacoes_extensao_ids, $avaliacao_ids);
             }
                         
-            $avaliacoes_extensao = Avaliacao::whereIn('id', $avaliacoes_extensao_ids)->orderBy('status')->paginate(5);
+            $avaliacoes_extensao = Avaliacao::whereIn('id', $avaliacoes_extensao_ids)->orderBy('status');
         }
 
         if (in_array(Dimensao::GESTAO, $dimensoes)) {
@@ -571,7 +570,7 @@ class PadController extends Controller
                 $avaliacoes_gestao_ids = array_merge($avaliacoes_gestao_ids, $avaliacao_ids);
             }
             
-            $avaliacoes_gestao = Avaliacao::whereIn('id', $avaliacoes_gestao_ids)->orderBy('status')->paginate(5);
+            $avaliacoes_gestao = Avaliacao::whereIn('id', $avaliacoes_gestao_ids)->orderBy('status');
         }
         
         
@@ -586,15 +585,15 @@ class PadController extends Controller
     private function get_carga_horaria_total($avaliacoes)
     {
         $ch = 0;
-        $avaliacoes_ensino   = $avaliacoes['ensino'];
-        $avaliacoes_pesquisa = $avaliacoes['pesquisa'];
-        $avaliacoes_extensao = $avaliacoes['extensao'];
-        $avaliacoes_gestao   = $avaliacoes['gestao'];
-        
+        $avaliacoes_ensino   = $avaliacoes['ensino']->get();
+        $avaliacoes_pesquisa = $avaliacoes['pesquisa']->get();
+        $avaliacoes_extensao = $avaliacoes['extensao']->get();
+        $avaliacoes_gestao   = $avaliacoes['gestao']->get();
+
         for ($i = 0; $i < count($avaliacoes_ensino->all()); $i++){
             $ch += $avaliacoes_ensino[$i]->tarefa()->first()->ch_semanal;
         }
-
+        
         for ($i = 0; $i < count($avaliacoes_pesquisa->all()); $i++){
             $ch += $avaliacoes_pesquisa[$i]->tarefa()->first()->ch_semanal;
         }
