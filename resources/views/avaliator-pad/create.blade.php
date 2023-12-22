937b423a7d791389d7626cdcf897e1d505792217
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2023-12-22 04:41:44 -0300

    update branch

--- resources/views/avaliator-pad/create.blade.php
+++ resources/views/avaliator-pad/create.blade.php
@@ -1,39 +1,5 @@
-
-@php
-    dd('create update');
-@endphp
-
 <div class="mb-3">
     <h3 class="h3"> Cadastrar Avaliador (PDA) </h3>
 </div>
 
-<form id="form-user_pad" action="{{ route('avaliator-pad_store') }}" method="POST">
-    @method('POST')
-    @csrf
 
-    @include('avaliator-pad.form', [
-        'pad' => $pad,
-        'model' => $model,
-        'users' => $users,
-        'status' => $status,
-    ])
-
-    <div class="mt-1 text-end">
-        <div class="modal-footer">
-            @include('components.buttons.btn-save', [
-                'id' => 'btn_submit',
-                'content' => 'Cadastrar',
-            ])
-
-            @include('components.buttons.btn-close_modal')
-        </div>
-    </div>
-
-</form>
-
-@include('pad.components.scripts.ajaxValidationById', [
-    'btn_submit_id' => 'btn_submit',
-    'form_id' => 'form-user_pad',
-    'form_type' => 'create_and_update',
-    'route' => route('avaliador-pad_ajax_validation'),
-])
