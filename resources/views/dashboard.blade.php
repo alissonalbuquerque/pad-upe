d947c77db1a3ef0fada8e010ac8bd4ed5ca33450
alissonalbuquerque <alissonalbuquerque.dev@gmail.com>
2024-03-05 13:40:26 -0300

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,7 +7,9 @@
             @endif
 
             @if(Auth::user()->isTypeTeacher())
-                @php dd('teacher'); @endphp
+                @php 
+                    ['user' => Auth::user(), 'userPads' => $userPads]
+                @endphp
                 @include('layouts.user-dashboard.dashboard_teacher', ['user' => Auth::user(), 'userPads' => $userPads])
             @endif
 
